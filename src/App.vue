678c453f66137a20b5b6134391e5302ad0df760e
Shane <sotiris4321@gmail.com>
2023-09-01 22:44:10 +0300

    working on starting the main layout of the app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app>
+    <AppHeader />
+    <v-main>
+      <router-view/>
+    </v-main>
+    <AppFooter />
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+
+import AppFooter from '@/components/AppFooter.vue';
+import AppHeader from '@/components/AppHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppFooter,
+    AppHeader,
+  },
+
+  data: () => ({
+    //
+  }),
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-toolbar
+      dark
+      prominent
+      image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'AppHeader',
+
+  components: {
+  },
+    data: () => ({
+    }),
+});
+</script>
