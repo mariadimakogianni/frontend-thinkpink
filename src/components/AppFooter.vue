d3af7e060e9eeed1f4cd7eaa25702a62cf456957
Sotiris Anagnostopoulos <sotiris@sexycoders.org>
2023-09-02 05:03:04 +0300

    footer-header

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,38 +1,79 @@
 <template>
-  <v-footer class="bg-indigo-lighten-1">
-    <v-row justify="center" no-gutters>
-      <v-btn
-        v-for="link in links"
-        :key="link"
-        color="white"
-        variant="text"
-        class="mx-2"
-        rounded="xl"
-      >
-        {{ link }}
-      </v-btn>
-      <v-col class="text-center mt-4" cols="12">
-        © 2019 - {{ new Date().getFullYear() }} — SEXYCODERS LIMITED
-      </v-col>
-    </v-row>
+  <v-footer class="pa-3">
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col
+          v-for="link in links"
+          :key="link"
+          class="caption text-center"
+          cols="auto"
+        >
+          <a href="#" class="styled-link">
+            {{ link }}
+          </a>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col class="caption text-center text--secondary">
+          © 2019 - {{ new Date().getFullYear() }} — YourCompanyName
+        </v-col>
+      </v-row>
+    </v-container>
   </v-footer>
 </template>
+
 <script>
 import { defineComponent } from 'vue';
-export default defineComponent({
-  name: 'HomeView',
 
-  components: {
-  },
-    data: () => ({
-      links: [
-        'Home',
-        'About Us',
-        'Team',
-        'Services',
-        'Blog',
-        'Contact Us',
-      ],
-    }),
+export default defineComponent({
+  name: 'AppFooter',
+  data: () => ({
+    links: [
+      'Home',
+      'About Us',
+      'Team',
+      'Services',
+      'Blog',
+      'Contact Us',
+    ],
+  }),
 });
 </script>
+
+<style scoped>
+  .v-footer {
+    background-color: white;
+    width: 100%;
+    max-width: 1200px;
+    margin: auto;
+    border-top: 1px solid #e0e0e0;
+  }
+
+  .styled-link {
+    text-decoration: none;
+    color: inherit;
+    padding: 0 8px;
+    position: relative;
+  }
+
+  .styled-link:hover {
+    text-decoration: none;
+  }
+
+  .styled-link::after {
+    content: '';
+    position: absolute;
+    bottom: -2px;
+    left: 0;
+    right: 0;
+    height: 2px;
+    background-color: var(--v-secondary-base);
+    opacity: 0;
+    transition: opacity 0.3s ease;
+  }
+
+  .styled-link:hover::after {
+    opacity: 1;
+  }
+</style>
+
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,30 +1,47 @@
 <template>
   <div>
-    <v-toolbar
+    <v-app-bar
       dark
-      prominent
-      image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+      flat
+      app
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="mr-5">YourAppName</v-toolbar-title>
 
-      <v-toolbar-title>Vuetify</v-toolbar-title>
+      <v-text-field
+        rounded
+        flat
+        solo-inverted
+        hide-details
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+      ></v-text-field>
 
       <v-spacer></v-spacer>
 
       <v-btn icon>
-        <v-icon>mdi-export</v-icon>
+        <v-icon>mdi-bell</v-icon>
       </v-btn>
-    </v-toolbar>
+
+      <v-btn icon>
+        <v-icon>mdi-account-circle</v-icon>
+      </v-btn>
+    </v-app-bar>
   </div>
 </template>
+
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+
 export default defineComponent({
   name: 'AppHeader',
+  setup() {
+    const drawer = ref(false);
 
-  components: {
+    return {
+      drawer,
+    };
   },
-    data: () => ({
-    }),
 });
 </script>
+
