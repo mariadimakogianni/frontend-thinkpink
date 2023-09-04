29ef39c59866b47c53f5025f3a8569403c4e8c5c
Sotiris Anagnostopoulos <sotiris@sexycoders.org>
2023-09-04 21:16:56 +0300

    fixed title and favicon

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,8 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import(/* webpackChunkName: "home" */ '../views/HomeView.vue')
+    component: () => import(/* webpackChunkName: "home" */ '../views/HomeView.vue'),
+    meta: { title: 'Home - SexyCoders' }
   },
   ]
 
@@ -12,5 +13,17 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+
+router.beforeEach((to, from, next) => {
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title)
+  const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.title)
+
+  if (nearestWithTitle) document.title = nearestWithTitle.meta.title
+  else if (previousNearestWithMeta) document.title = previousNearestWithMeta.meta.title
+
+  next()
+})
+
 
 export default router
