93bad6076754240199cd1c9cdb77c6c4d965037f
Sotiris Anagnostopoulos <sotiris@sexycoders.org>
2023-09-04 15:12:33 +0300

    made great progress in info section - added dual panes, added  maps, fixed form, added validation - documentation link

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <AppHeader />
     <HomeSection />
     <HomeAbout />
+    <ContactSection />
     <AppFooter />
   </div>
 </template>
@@ -12,12 +13,14 @@ import AppHeader from '@/components/AppHeader.vue';
 import HomeSection from '@/components/HomeSection.vue';
 import AppFooter from '@/components/AppFooter.vue';
 import HomeAbout from '@/components/HomeAbout.vue';
+import ContactSection from '@/components/ContactSection.vue';
 
 export default {
   name: 'HomeView',
   components: {
     AppHeader,
     HomeSection,
+    ContactSection,
     AppFooter,
     HomeAbout,
   },
